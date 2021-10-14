c15551c96be35520d68eda91e1556b71fbb8f8e4
冯廉义 <Fly1324928301@users.noreply.github.com>
2021-10-14 14:38:50 +0800

    Update dingding.sh

--- dingding.sh
+++ dingding.sh
@@ -3,4 +3,4 @@ set -eux
 
 curl 'https://oapi.dingtalk.com/robot/send?access_token=268c0abbf8f7d17dd32cc2a01821e3664d3ba13a180d917d76d9b27b9bd695cc' \
  -H 'Content-Type: application/json' \
- -d '{"msgtype": "text","text": {"content":"吃饭，吃饭，@所有人 "}}'
+ -d '{"msgtype": "text","text": {"content":"吃饭，吃饭，@何攀 "}}'
