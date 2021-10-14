868ceef3d0cf7f8028328c3c8cca47e635597bf8
冯廉义 <Fly1324928301@users.noreply.github.com>
2021-10-14 11:59:33 +0800

    Create dingding.sh

---
+++ dingding.sh
@@ -0,0 +1,3 @@
+curl 'https://oapi.dingtalk.com/robot/send?access_token=268c0abbf8f7d17dd32cc2a01821e3664d3ba13a180d917d76d9b27b9bd695cc' \
+ -H 'Content-Type: application/json' \
+ -d '{"msgtype": "text","text": {"content":"吃饭，吃饭，@all"}}'
